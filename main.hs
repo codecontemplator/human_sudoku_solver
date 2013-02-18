ed0d4abd5b2746632cc98bda3ae282108d0b76d1
Daniel <->
2013-02-18 23:01:32 +0100

    .

--- main.hs
+++ main.hs
@@ -129,7 +129,7 @@ solve_internal board named_strategies =
 		apply_strategies_once board ((name,strategy):rest) =
 			case strategy board of
 				[] -> apply_strategies_once board rest
-				cells -> Just (name, replace_by_position board cells)
+				cells -> trace (show cells) $ Just (name, replace_by_position board cells)
 		apply_strategies :: Board -> [String] -> Maybe([String],Board)
 		apply_strategies board names = 
 			case apply_strategies_once board named_strategies of
@@ -137,7 +137,7 @@ solve_internal board named_strategies =
 					trace name $
 						let names' = names ++ [name] in
 						if is_complete board' then Just (names', board') else apply_strategies board' names'
-				_ -> trace (show board) Nothing
+				_ -> trace (board2string board) Nothing
 
 solve  :: Board -> Maybe ([String], Board)
 solve board = solve_internal board strategies
